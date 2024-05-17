e2fad5c9ff36248258598e2fa88ed9ddb08a3dfb
ParkMina04 <102229184+ParkMina04@users.noreply.github.com>
2024-05-17 21:19:16 +0300

    Add files via upload

---
+++ admin/admin.js
@@ -0,0 +1,11 @@
+const button = document.querySelector("button");
+const admin_phone = document.querySelector('.input-phone');
+const admin_password = document.querySelector('.input-pass');;
+
+button.onclick = function () {
+    // alert("Hello");
+    alert( admin_phone +" "+ admin_password);
+    // if(document.getElementsByClassName("input-phone").value ==="adminAdmin",document.getElementsByClassName("input-pass").value ==="adminAdmin"){
+            
+    // };
+};
